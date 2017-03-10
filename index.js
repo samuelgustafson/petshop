9d1f3f3aec59f3151abd88beffd30ed83672a6f8
sam gustafson <same@sames-Air.itpro.tv>
2017-03-09 19:51:57 -0500

    fixing configuration shananigans

--- index.js
+++ index.js
@@ -44,7 +44,19 @@ server.get('/animals', function(req,res){
   });
 });
 //Get /animals/:id
-server.get('/animals/:id', function(req,res){});
+server.get('/animals/:id', function(req,res){
+  Animal.find({_id: req.params.id}, function(err, documents){
+    if(err){
+      res.status(500).json({
+        msg:err
+      });
+    } else {
+      res.status(200).json({
+        animals: documents
+      });
+    }
+  });
+});
 //POST /animals
 //PUT /animals/:id
 //DELETE /animals/:id
